3327673fde865d04ca798c4224f7f384ced3d2c6
Niki Arrogante <s3851498@student.rmit.edu.au>
2020-09-28 21:04:58 +1000

    Free movement alpha
    - able to move horizontally now
    - need to restrict free movement on each side
    - need to find a way for paddle speed to affect ball speed

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Ball : MonoBehaviour 
+public class Ball : MonoBehaviour
 {
-
     // Access
     private Rigidbody2D rigidBall;
 
@@ -12,7 +9,7 @@ public class Ball : MonoBehaviour
     public int ballForce = 30;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rigidBall = GetComponent<Rigidbody2D>();
         Invoke("LaunchSelect", 2);
@@ -24,50 +21,52 @@ public class Ball : MonoBehaviour
         {
             Vector2 velocity;
             velocity.x = rigidBall.velocity.x;
+
             velocity.y = (rigidBall.velocity.y / 2) + (collision.collider.attachedRigidbody.velocity.y / 3);
             rigidBall.velocity = velocity;
         }
     }
 
     // Puts the ball in center (Refactor later for setting to player paddle etc)
-    void ResetToCeter()
+    private void ResetToCeter()
     {
         rigidBall.velocity = Vector2.zero;
         transform.position = Vector2.zero;
     }
 
-    void RestartGame()
+    private void RestartGame()
     {
         ResetToCeter();
-        Invoke("LaunchSelect", 2);
+        Invoke("LaunchSelect", 1);
     }
 
-/*    void Invoke()
-    {
-        LaunchSelect();
-    }*/
+    /*    void Invoke()
+        {
+            LaunchSelect();
+        }*/
 
     // Choose if a direction or a random launch
-    void LaunchSelect()
+    private void LaunchSelect()
     {
         RandomLaunch();
     }
 
     // Send launch direction
-    void LaunchBall(int x, int y)
+    private void LaunchBall(int x, int y)
     {
         rigidBall.AddForce(new Vector2(x, y));
     }
 
     // This is the default for now
-    void RandomLaunch()
+    private void RandomLaunch()
     {
         float randomX = Random.Range(0, 2);
         int randomY = Random.Range(-15, 15);
         if (randomX < 1)
         {
             LaunchBall(ballForce, randomY);
-        } else
+        }
+        else
         {
             LaunchBall(-ballForce, randomY);
         }
--- Pong/Assets/Scripts/PlayerControls.cs
+++ Pong/Assets/Scripts/PlayerControls.cs
@@ -3,12 +3,13 @@
 public class PlayerControls : MonoBehaviour
 {
     // Enter the axis name found in Input Manager here, which signifies which player is being controlled
-    public string playerNumber;
+    public string playerVert;
+    public string playerHori;
 
     // Game experience
     public float speed = 10.0f;
 
-    public float boundary = 4.09f;
+    public float boundary = 5.5f;
 
     // Access
     private Rigidbody2D rigidPaddle;
@@ -27,28 +28,12 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //var velocity = rigidPaddle.velocity;
-
         // Finds the axis name set in inspector and uses it to find the input being used
-        float vert = Input.GetAxis(playerNumber);
+        float vert = Input.GetAxis(playerVert);
+        float hori = Input.GetAxis(playerHori);
 
         // "Translates" the sum of y-axis movement x speed x delta and turns it into movement.
-        transform.Translate(new Vector2(0, vert) * speed * Time.deltaTime);
-
-        // transform.Translate line of code replaces the below velocity code
-        //if (vertical > .1)
-        //{
-        //    velocity.y = speed;
-        //}
-        //else if (vertical < -.1)
-        //{
-        //    velocity.y = -speed;
-        //}
-        //else
-        //{
-        //    velocity.y = 0;
-        //}
-        //rigidPaddle.velocity = velocity;
+        transform.Translate(new Vector2(hori, vert) * speed * Time.deltaTime);
 
         var position = transform.position;
         if (position.y > boundary)
@@ -59,6 +44,16 @@ public class PlayerControls : MonoBehaviour
         {
             position.y = -boundary;
         }
+
+        if (position.x > 9)
+        {
+            position.x = 9;
+        }
+        else if (position.x < -9)
+        {
+            position.x = -9;
+        }
+
         transform.position = position;
     }
 }
